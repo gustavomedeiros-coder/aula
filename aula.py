3923f1f04a2b30a0f3f8df99050d5df071d6bf87
gustavomedeiros-coder <gustavogmedeiros2018@gmail.com>
2026-08-03 17:55:47 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -31,3 +31,24 @@ df = carregar_dados()
 st.subheader("Dados")
 
 st.dataframe(df.head())
+
+#a partir daqui são gráficos
+st.subheader("Série temporal")
+
+fig, ax = plt.subplots(figsize=(12,6))
+
+ax.plot(
+    df["data"],
+    df["valor"]
+)
+
+ax.set_title(
+    "Série temporal"
+)
+
+ax.grid(
+    linestyle="--",
+    alpha=0.3
+)
+
+st.pyplot(fig)
