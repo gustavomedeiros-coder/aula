a3f17c97b5e4ee24af412ca660e4696ae77cccf1
gustavomedeiros-coder <gustavogmedeiros2018@gmail.com>
2026-08-03 17:52:57 -0300

    Update aula.py

--- aula.py
+++ aula.py
@@ -1,5 +1,33 @@
 import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
 
-st.title("Meu Dashboard")
 
-st.write("Meu primeiro dashboard usando Streamlit!")
+# Configuração da página
+st.set_page_config(
+    page_title="Meu Dashboard",
+    layout="wide"
+)
+
+
+# Título
+st.title("📊 Meu Dashboard")
+
+
+# Carregar dados
+url = ""
+
+
+@st.cache_data
+def carregar_dados():
+    df = pd.read_csv(url)
+    return df
+
+
+df = carregar_dados()
+
+
+# Visualização inicial
+st.subheader("Dados")
+
+st.dataframe(df.head())
